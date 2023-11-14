e49dad22e2a2eccb7a790465068679227465bef9
bekatru <teamusicband@gmail.com>
2023-11-14 23:28:43 +0600

    Remove lights from code, add light to the scene, import updated scene

--- index.js
+++ index.js
@@ -5,32 +5,18 @@ import {
 import {
   GLTFLoader
 } from 'three/addons/loaders/GLTFLoader.js';
-import {
-  HemisphereLightHelper,
-  PointLightHelper,
-  SpotLightHelper
-} from "three";
 
-
-const SHOW_GRID = false
-const SHOW_LIGHT_HELPERS = false
 
 const scene = new THREE.Scene();
 
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
+camera.position.y = 2
 
-if (SHOW_GRID) {
-  const size = 8;
-  const divisions = 8;
-  const gridHelper = new THREE.GridHelper(size, divisions);
-
-  scene.add(gridHelper);
-}
-
-//Renderer
-const renderer = new THREE.WebGLRenderer({antialias: true});
+const renderer = new THREE.WebGLRenderer({
+  antialias: true,
+  ambientOcclusion: true
+});
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -51,15 +37,34 @@ loader.load(
   './assets/models/homeoffice.glb',
   function (gltf) {
     const model = gltf.scene
+
+    const point = model.children
+      .find(({name}) => name === 'Point').children
+      .find(({name}) => name === 'Point_Orientation')
+
+    point.castShadow = true
+    point.power = 60
+    point.shadow.bias = -0.0001
+
+
+    const lampRight = model.children
+      .find(({name}) => name === 'LampRight').children
+      .find(({name}) => name === 'LampRight_Orientation')
+
+    lampRight.castShadow = true
+    lampRight.power = 10
+    lampRight.shadow.bias = -0.0001
+
+
+    const lampLeft = model.children
+      .find(({name}) => name === 'LampLeft').children
+      .find(({name}) => name === 'LampLeft_Orientation')
+
+    lampLeft.castShadow = true
+    lampLeft.power = 10
+    lampLeft.shadow.bias = -0.0001
+
     model.traverse(function (node) {
-      const wallsObject = model.children.find(({name}) => name === 'Walls')
-      const ceilingLamp = model.children.find(({name}) => name === 'CeilingLampBottom')
-      const coffeeTable = model.children.find(({name}) => name === 'CoffeeTable')
-      coffeeTable.castShadow = true
-      coffeeTable.receiveShadow = false
-      ceilingLamp.castShadow = false
-      ceilingLamp.receiveShadow = false
-      wallsObject.castShadow = false
       if (node.isMesh) {
         node.castShadow = true;
         node.receiveShadow = true
@@ -78,50 +83,6 @@ loader.load(
     console.error(error);
   }
 );
-
-
-const spotLight = new THREE.SpotLight(0xFFEDCD)
-const spotLightHelper = new SpotLightHelper(spotLight)
-spotLight.position.set(0, 2.5, 2.2)
-spotLight.angle = Math.PI / 2.3
-spotLight.power = 100
-spotLight.penumbra = 1
-spotLight.castShadow = true
-spotLight.shadow.bias = -0.0005
-
-const hemisphereLight = new THREE.HemisphereLight(0xFFEDCD)
-const hemisphereLightHelper = new HemisphereLightHelper(hemisphereLight)
-hemisphereLight.position.set(0, 2.5, 2.2)
-hemisphereLight.intensity = 0.5
-
-const pointLight = new THREE.PointLight(0xFFEDCD)
-const pointLightHelper = new PointLightHelper(pointLight, 0.2)
-pointLight.position.set(1.45, 1, -1)
-pointLight.castShadow = true
-pointLight.intensity = 0.5
-
-const pointLight2 = new THREE.PointLight(0xFFEDCD)
-const pointLightHelper2 = new PointLightHelper(pointLight2, 0.2)
-pointLight2.position.set(-2.2, 0.9, -0.48)
-pointLight2.castShadow = true
-pointLight2.intensity = 0.5
-
-
-scene.add(
-  spotLight,
-  hemisphereLight,
-  pointLight,
-  pointLight2
-)
-
-if (SHOW_LIGHT_HELPERS) {
-  scene.add(
-    spotLightHelper,
-    hemisphereLightHelper,
-    pointLightHelper,
-    pointLightHelper2
-  )
-}
 
 const clock = new THREE.Clock()
 
